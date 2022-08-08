7de496c38e89ab2c574041159e87cc0f876b2781
Mykyta Gazul <nikitagazul@gmail.com>
2022-08-08 01:18:40 -0400

    main component styling done

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1291194418msh37267f1410f3ad4p1c90eejsn87e0371c3331',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
